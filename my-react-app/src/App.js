72331652cadd36347dcd372b65288eaf15124a14
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-03 14:56:41 +0700

    first commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import "./App.css"
+import AddUser from './Components/Users/AddUser';
+import UsersList from './Components/Users/UsersList';
+
+
+
+function App() {
+  const [usersList, setUsersList] = useState([{name: "Jone Geadtr", age: 30}]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
