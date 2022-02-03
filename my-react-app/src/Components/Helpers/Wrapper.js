2ac2ca2119a098e1f3392d0033f1b52e9a46c43e
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-03 15:10:25 +0700

    Creating a Wrapper Component

---
+++ my-react-app/src/Components/Helpers/Wrapper.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Wrapper.module.css";
+
+const Wrapper = (props) => {
+    return props.children;
+}
+
+export default Wrapper;
--- my-react-app/src/Components/Users/AddUser.js
+++ my-react-app/src/Components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import Button from "../UI/Button/Button";
 import classes from "./AddUser.module.css";
 import ErrorModle from "../UI/ErrorModle";
+import Wrapper from "../Helpers/Wrapper"
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
@@ -43,7 +44,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModle
           title={error.title}
@@ -70,7 +71,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
